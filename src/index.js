1fedd1a0b423b2bec5b9cc07895bbf8c950ae33f
qkrthdus605 <qkrthdus605@gmail.com>
2021-01-14 21:52:43 +0900

    Movie list API 로딩(axios사용)

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const formatName = function(name){
+  return name.firstName + " " + name.lastName;
+}
+
+const name = {
+  firstName: "Soyeon",
+  lastName: "Park"
+};
+const element = <h1>Hello, {formatName(name)}!</h1>
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +19,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
