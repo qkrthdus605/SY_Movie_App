95c97fb24db16fd9eb8619b9a69395150bd6cde0
qkrthdus605 <qkrthdus605@gmail.com>
2021-01-15 23:14:40 +0900

    title, year, summary 출력(이미지 출력 안됨)

--- src/Movie.js
+++ src/Movie.js
@@ -1,9 +1,17 @@
 // Movie Render
 import React from "react";
 import PropTypes from "prop-types";
+import "./Movie.css";
 
-function Movie({id, year, title, summary, poster}){
-    return <h1>{title}</h1>;
+function Movie({year, title, summary, poster}){
+    return <div class="movie">
+        <img scr={poster} alt={title} title={title} />
+        <div class="movie_data">
+            <h3 class="title">{title}</h3>
+            <h5 class="year">{year}</h5>
+            <p class="summary">{summary}</p>
+        </div>
+    </div>
 }
 
 Movie.propTypes={
