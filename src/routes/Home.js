6a38348761ffbc267c74d48ac64ed125362993a0
qkrthdus605 <qkrthdus605@gmail.com>
2021-01-17 17:45:58 +0900

    router 추가

---
+++ src/routes/Home.js
@@ -0,0 +1,50 @@
+import React from "react";
+import axios from "axios";
+import Movie from "../components/Movie";
+import "./Home.css";
+
+class Home extends React.Component{
+  state = {
+    isLoading: true,
+    movies: []
+  };
+  // axios가 끝날 때까지 기다렸다가 진행(async, await)
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies }
+       }
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    this.setState({ movies, isLoading: false });
+  };
+  componentDidMount(){
+     this.getMovies();
+  }
+  render(){
+    const{ isLoading, movies } = this.state;
+    return(
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loading_text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map(movie => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image} 
+                genres={movie.genres} 
+            />
+          ))}
+          </div>
+      )}
+      </section>
+    );
+  }
+}
+export default Home;
