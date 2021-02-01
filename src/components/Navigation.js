64dd2f84f769e9a9fbbad4cdf404473d2f16dfc4
qkrthdus605 <qkrthdus605@gmail.com>
2021-02-01 18:58:44 +0900

    navigation 경로 수정

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,7 +6,6 @@ function Navigation(){
     return(
         <div className="nav">
             <p><Link to="/" className="home">Home</Link></p>
-            <p><Link to="/about" className="about">About</Link></p>
             <p><Link to="./search" className="search">Search</Link></p>
         </div>
     ); 
--- src/index.js
+++ src/index.js
@@ -2,20 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-const formatName = function(name){
-  return name.firstName + " " + name.lastName;
-}
-
-const name = {
-  firstName: "Soyeon",
-  lastName: "Park"
-};
-const element = <h1>Hello, {formatName(name)}!</h1>
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
+ReactDOM.render(<App />, document.getElementById('root'));
